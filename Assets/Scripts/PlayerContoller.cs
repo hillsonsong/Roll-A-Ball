d138b6205d830ede6a5749410dd3e7f207c8e629
Hillson <sxcheen@gmail.com>
2016-04-18 22:20:12 +0800

    add ingore; finished game

--- Assets/Scripts/PlayerContoller.cs
+++ Assets/Scripts/PlayerContoller.cs
@@ -1,15 +1,22 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
 using System.Collections;
 
 public class PlayerContoller : MonoBehaviour {
 
 	public float speed;//速度
+	public Text countText;
+	public Text winText;
 
 	private Rigidbody rb;
+	private int count;
 
 	// Use this for initialization
 	void Start () {
 		rb = GetComponent<Rigidbody> ();
+		count = 0;
+		SetCountText ();
+		winText.text = "";
 	}
 	
 	// Update is called once per frame
@@ -23,6 +30,22 @@ public class PlayerContoller : MonoBehaviour {
 
 		Vector3 movement = new Vector3 (moveHorizontal, 0.0f, moveVertical);
 
-		rb.AddForce (movement * 2);
+		rb.AddForce (movement * 5);
+	}
+
+	void OnTriggerEnter(Collider other) {
+//		Destroy (other.gameObject);
+		if (other.gameObject.CompareTag ("Pick Up")) {
+			other.gameObject.SetActive (false);
+			count++;
+			SetCountText ();
+			if(count >= 12) {
+				winText.text = "You Win~!";
+			}
+		}
+	}
+
+	void SetCountText() {
+		countText.text = "Count : " + count.ToString ();
 	}
 }
